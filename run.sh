824e0f4c8dbcdd5cfe49a246733ad4fa7f56449c
Xiaojun Hong <zootim11pc@gmail.com>
2022-11-16 14:19:59 +0800

    更新腳本

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-php artisan config:cache --no-interaction && php artisan view:cache --no-interaction
+php artisan config:cache --no-interaction && php artisan view:cache --no-interaction && php artisan route:cache
 
 php-fpm &
 nginx -g "daemon off;"
